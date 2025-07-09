bd82ddbc2f112994bb2d3234838c4c815d37a0dc
Yatsenko Pavlo <pavelyasenko18@gmail.com>
2025-07-09 20:08:20 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ export const alex = {
 
 export const App = () => (
   <div className="App">
-    <Person person={misha} age={37} />
-    <Person person={olya} age={0} />
-    <Person person={alex} age={25} />
+    <Person person={misha} />
+    <Person person={olya} />
+    <Person person={alex} />
   </div>
 );
